940a2e1e9f8251a2a932b03e9b88c8f50fab319f
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2024-06-19 11:37:17 +0200

    :chart:

---
+++ php/topbar.php
@@ -0,0 +1,16 @@
+<?php
+function topbar()
+{
+    echo "<div class=\"header\">
+        <div class=\"header_left\">
+            <div class=\"header_logo\"><a href=\"#\"><img src=\"./img/zwitscher_simple_logo0.png\"alt=\"Logo\"></a></div>
+            <div class=\"header_like\"><a href=\"#\"><img src=\"./img/like.png\" alt=\"Like\"></a></div>
+        </div>
+        <div class=\"header_right\">
+            <div class=\"header_pfp\"> <a href=\"./sub/register.php\"><img src=\"./img/defult_pfp.png\" alt=\"pfp\"></a></div>
+            <div class=\"header_impressum\"><a href=\"#\">Impressum</a></div>
+        </div>
+    </div>";
+}
+
+?>
---
+++ sub/register.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Zwicher</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/main.css">
+</head>
+
+<body>
+    <dvi class ="register">
+        <form action="register.php" method="post">
+            <label for="username">Username:</label><br>
+            <input class="register_input" id="username" name="username" required type="text" /><br>
+            <label for="email">Email:</label><br>
+            <input class="register_input" id="email" name="email" required type="email" /><br>
+            <label for="password">Password:</label><br>
+            <input class="register_input" id="password" name="password" required type="password" /><br>
+            <div class="register_buttons">
+            <input class="register_send" name="register" type="submit" value="Register" />
+            <div class="register_login"><a href="./login">Login</a></div></divclass>
+        </form>
+    </dvi>
+</body>
+
+</html>
