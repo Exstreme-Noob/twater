7e743c6896c36ab73c02763871dcd21d80566261
Exstreme_Noob <116436587+Exstreme-Noob@users.noreply.github.com>
2024-07-04 09:48:34 +0200

    :a:

--- handle_eddit_profile.php
+++ handle_eddit_profile.php
@@ -12,17 +12,17 @@
 
     if ($_POST["password"]!=NULL) {
         $password = $_POST["password"];
-        $sql = "UPDATE users SET `password`='$password' WHERE uid = $uid;";
+        $sql = "UPDATE `users` SET `password`='$password' WHERE uid = $uid;";
         getSQLQuery($sql);
     }
 
     if ($_POST["email"]!=NULL) {
         $email = $_POST["email"];
-        $sql = "SELECT uid FROM users WHERE email = $email;";
+        $sql = "SELECT `uid` FROM `users` WHERE `email` = $email;";
         $oldmail = mysqli_fetch_array(getSQLQuery($sql))[0];
         if ($oldmail == $uid) {
             $_SESSION["email"]=$email;
-            $sql = "UPDATE users SET email='$email' WHERE uid = $uid;";
+            $sql = "UPDATE `users` SET `email`='$email' WHERE `uid` = $uid;";
             getSQLQuery($sql);
         }
         else{
